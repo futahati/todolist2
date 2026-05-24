2a0ad0e66ea32ee59c6a512f3c99d14a65f55290
irw <futahati@gmail.com>
2026-05-24 14:32:21 +0800

    部分完成list.html

--- todos/urls.py
+++ todos/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-from users import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("todos.urls")),  # 首頁不用打倒斜線
-    path("users/profile/", views.profile),  # flask倒斜線在前，Django倒斜線在後
+    path(
+        "", views.todo_list
+    ),  # 首頁不用打倒斜線；有名稱時，flask倒斜線在前，Django倒斜線在後
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
+
+# from django.http import JsonResponse
+
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    print(todos)
+
+    return render(request, "todos/list.html", {"todos": todos})
