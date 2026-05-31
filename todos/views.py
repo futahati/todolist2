da96003afca62050583e6552a61df9f83ff932ac
irw <futahati@gmail.com>
2026-05-31 11:28:11 +0800

    完成update.html

--- todos/views.py
+++ todos/views.py
@@ -25,6 +25,23 @@ def todo_delete(request, id):
     return redirect("todo-list")
 
 
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    message = None
+
+    if request.method == "GET":
+        # instance將資料填入TodoForm
+        form = TodoForm(instance=todo)
+    elif request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            print("更新 Todo 完成！")
+            message = "更新 Todo 完成！"
+
+    return render(request, "todos/update.html", {"form": form, "message": message})
+
+
 def todo_create(request):
 
     if request.method == "POST":
